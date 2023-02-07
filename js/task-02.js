9c83e5802d02cac9ee46a0e8cf285b1e68257aef
Max Rostipira <maxkrovat2@gmail.com>
2023-02-07 15:59:17 +0200

    1WEQWE

---
+++ js/task-02.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+
+const 
+
+
+const Items = document.createElement('li')
+Items.textContent = "Potatoes";
